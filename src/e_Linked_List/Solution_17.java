882baeb9929ab6663c2ebb48ccaae7ec3392cea2
Raghav Rana <raghavrana2509@gmail.com>
2026-02-14 02:07:58 +0530

    feb.14.2026

---
+++ src/e_Linked_List/Solution_17.java
@@ -0,0 +1,57 @@
+package e_Linked_List;
+
+public class Solution_17 {
+    public static ListNode deleteMiddle(ListNode head) {
+        if(head == null || head.next == null) return null;
+
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode prev = null;
+
+        while(fast != null && fast.next != null){
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        prev.next = slow.next;
+
+        return head;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50};
+        ListNode head = createList(arr);
+// Test your function
+        printList(deleteMiddle(head));
+    }
+
+    static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int val) {
+            this.val = val;
+            this.next = null;
+        }
+    }
+    // Create Linked List from array
+    public static ListNode createList(int[] arr) {
+        if (arr.length == 0) return null;
+        ListNode head = new ListNode(arr[0]);
+        ListNode temp = head;
+        for (int i = 1; i < arr.length; i++) {
+            temp.next = new ListNode(arr[i]);
+            temp = temp.next;
+        }
+        return head;
+    }
+    // Print Linked List
+    public static void printList(ListNode head) {
+        ListNode temp = head;
+        while (temp != null) {
+            System.out.print(temp.val + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+}
